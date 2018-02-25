87abd60a5f4948093ee45a0927f02fafd6ebd9a0
Falzed <oskari_kulmala@hotmail.com>
2018-02-25 14:51:38 +0200

    4.10, 4.11

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -16,11 +16,15 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    if (request.body.title === undefined) {
+    if (request.body.title === undefined ||
+        request.body.url === undefined) {
         return response.status(400).json({ error: 'no title' })
     }
 
     const blog = new Blog(request.body)
+    if (blog.likes === undefined) {
+        blog.likes = 0
+    }
 
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
--- tests/blog_api_post.test.js
+++ tests/blog_api_post.test.js
@@ -6,32 +6,88 @@ const api = supertest(app)
   await Blog.remove({})
 }) */
 
-test('adding blog works', async () => {
-    const newBlog = {
-        "title": "Type wars",
-        "author": "Robert C. Martin",
-        "url": "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        "likes": 2,
-    }
-    const response = await api
-        .get('/api/blogs')
-    const alku = response.body.length
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('POST:', async () => {
+    test('adding blog works', async () => {
+        const newBlog = {
+            "title": "Type wars",
+            "author": "Robert C. Martin",
+            "url": "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            "likes": 2,
+        }
+        const response = await api
+            .get('/api/blogs')
+        const alku = response.body.length
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
 
-    const responseAfter = await api
-        .get('/api/blogs')
-    const titles = responseAfter.body.map(blog => blog.title)
+        const responseAfter = await api
+            .get('/api/blogs')
+        const titles = responseAfter.body.map(blog => blog.title)
 
-    expect(responseAfter.body.length).toBe(alku + 1)
-    expect(titles[0]).toBe("React patterns")
-    expect(titles[alku]).toBe("Type wars")
-})
+        expect(responseAfter.body.length).toBe(alku + 1)
+        expect(titles[alku]).toBe("Type wars")
+    })
 
-afterAll(() => {
-    server.close()
+    test('adding a blog with undefined likes is added with 0 likes', async () => {
+        const newBlog = {
+            "title": "No one likes me",
+            "author": "me",
+            "url": "http://cry.cry"
+        }
+        const response = await api
+            .get('/api/blogs')
+        const alku = response.body.length
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+
+        const responseAfter = await api
+            .get('/api/blogs')
+        const titles = responseAfter.body.map(blog => blog.title)
+
+        expect(responseAfter.body.length).toBe(alku + 1)
+        expect(titles[alku]).toBe("No one likes me")
+        expect(responseAfter.body[alku].likes).toBe(0)
+    })
+
+    test('adding a blog with no title responds 400', async () => {
+        const newBlog = {
+            "author": "Robert C. Martin",
+            "url": "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            "likes": 2,
+        }
+        const response = await api
+            .get('/api/blogs')
+        const alku = response.body.length
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+
+    test('adding a blog with no url responds 400', async () => {
+        const newBlog = {
+            "title": "URL-less",
+            "author": "Robert C. Martin",
+            "likes": 2,
+        }
+        const response = await api
+            .get('/api/blogs')
+        const alku = response.body.length
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+
+    afterAll(() => {
+        server.close()
+    })
 })
