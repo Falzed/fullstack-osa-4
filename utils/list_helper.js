93bae7b6b7c0dde062dc8072f8ed198511163af8
Falzed <oskari_kulmala@hotmail.com>
2018-02-25 11:56:29 +0200

    apufunktiot (4.5, 4.6, 4.7)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,11 +6,70 @@ const totalLikes = (blogs) => {
     let summa = 0
     blogs.forEach(blog => {
         summa += blog.likes
-    });
+    })
     return summa
+}
+
+const favoriteBlog = (blogs) => {
+    let suurinTykkaysMaara = 0
+    let suosituinBlogi = null
+    blogs.forEach(blog => {
+        if (blog.likes > suurinTykkaysMaara) {
+            suurinTykkaysMaara = blog.likes
+            suosituinBlogi = blog
+        }
+    })
+    if (suosituinBlogi === null) {
+        return null
+    }
+    return {
+        title: suosituinBlogi.title,
+        author: suosituinBlogi.author,
+        likes: suosituinBlogi.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    const authorsDictionary = {}
+    let suurinBlogiMaara = 0
+    let mahtavinBlogaaja = null
+    blogs.forEach(blog => {
+        if (authorsDictionary[blog.author] === undefined) {
+            authorsDictionary[blog.author] = 1
+        } else {
+            authorsDictionary[blog.author] = authorsDictionary[blog.author] + 1
+        }
+        if (authorsDictionary[blog.author] > suurinBlogiMaara) {
+            suurinBlogiMaara = authorsDictionary[blog.author]
+            mahtavinBlogaaja = blog.author
+        }
+    })
+    return mahtavinBlogaaja
+}
+
+const mostLikes = (blogs) => {
+    const authorsDictionary = {}
+    let suurinTykkaysMaara = 0
+    let mahtavinBlogaaja = null
+    blogs.forEach(blog => {
+        if (authorsDictionary[blog.author] === undefined) {
+            authorsDictionary[blog.author] = blog.likes
+        } else {
+            authorsDictionary[blog.author] = authorsDictionary[blog.author]
+                + blog.likes
+        }
+        if (authorsDictionary[blog.author] > suurinTykkaysMaara) {
+            suurinTykkaysMaara = authorsDictionary[blog.author]
+            mahtavinBlogaaja = blog.author
+        }
+    })
+    return mahtavinBlogaaja
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
