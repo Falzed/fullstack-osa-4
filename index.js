be87bf56993abd1681ed3d3651872f9b677e86e7
Falzed <oskari_kulmala@hotmail.com>
2018-02-25 10:57:14 +0200

    4.2

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog.js')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
-
+app.use('/api/blogs', blogsRouter)
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -25,24 +19,6 @@ if (process.env.NODE_ENV !== 'production') {
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
