5c01bd2ed3a997e4c0d5a4556a13dc94c65dd762
Falzed <oskari_kulmala@hotmail.com>
2018-02-25 11:18:53 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let summa = 0
+    blogs.forEach(blog => {
+        summa += blog.likes
+    });
+    return summa
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
